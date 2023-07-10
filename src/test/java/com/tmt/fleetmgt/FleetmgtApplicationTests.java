3f0bc1f5266a15541cef7827d223c9a30b83ec32
Aisha Mohammed <80531561+aisha-rm@users.noreply.github.com>
2023-07-10 01:07:12 +0100

    First Commit

---
+++ src/test/java/com/tmt/fleetmgt/FleetmgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmt.fleetmgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetmgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
